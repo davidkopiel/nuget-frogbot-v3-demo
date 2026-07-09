4d90fda40400d2f3fce0dcf637885598b7a76480
David Kopiel <davidko@jfrog.com>
2026-07-09 10:35:18 +0300

    Add a known-vulnerable NuGet package
    System.Text.Encodings.Web 4.5.0 has a public critical severity
    advisory (GHSA-ghhp-997w-qr28). Used alongside the self-compiled,
    non-public dependency to compare how Frogbot reports a package with
    known public CVE data versus one it has no data on at all.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
-﻿using Davidko.InternalUtils;
+﻿using System.Text.Encodings.Web;
+using Davidko.InternalUtils;
 using Newtonsoft.Json;
 
 var payload = new { message = "Hello from NugetFrogbotV3Demo", timestamp = DateTime.UtcNow };
 Console.WriteLine(JsonConvert.SerializeObject(payload));
 Console.WriteLine(Greeter.Greet("Frogbot"));
+Console.WriteLine(HtmlEncoder.Default.Encode("<script>demo</script>"));
